4612d90d0c59e262e0c7b5d74564ef0ee75be2d5
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-11-28 21:21:35 -0500

    most of the make move method done, worked on this for about an hour  -  moving on to study for tomorrow's assessment.

--- W4D2/skeleton/lib/board.rb
+++ W4D2/skeleton/lib/board.rb
@@ -2,16 +2,42 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { Array.new }
+    place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    i = 0 
+    while i < 14
+      @cups[i] = [:stone, :stone, :stone, :stone] if ( i != 6 && i != 13 )
+      i += 1
+    end
   end
 
   def valid_move?(start_pos)
+    raise 'Invalid starting cup' if start_pos > 14 || start_pos < 0
+    raise 'Starting cup is empty' if @cups[start_pos].empty?
+    true
   end
 
   def make_move(start_pos, current_player_name)
+    if valid_move?(start_pos)
+
+      stones = @cups[start_pos].length
+      @cups[start_pos] = []
+
+      while stones > 0
+        start_pos += 1
+        start_pos = start_pos % 14
+        start_pos += 1 if (start_pos == 13)
+        start_pos = start_pos % 14
+        @cups[start_pos] << :stone
+        stones -= 1
+      end
+
+      
+      render
+    end
   end
 
   def next_turn(ending_cup_idx)
