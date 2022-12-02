0e70e0d5c480f0e30da0ea6f7c7817b01c6a8934
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-12-01 22:30:06 -0500

    finished like_a_cloctopus method -- linear iteration

--- W4D4/octopus.rb
+++ W4D4/octopus.rb
@@ -10,12 +10,17 @@ end
 
 
 def dOm(fish_arr)
-
+    mid = fish_arr.length / 2
+    fish_arr[mid]
 
 end
 
 
 def like_a_cloctopus(fish_arr)
+    max_fish = ''
 
-    
+    fish_arr.each do |fish|
+        max_fish = fish if max_fish.length < fish.length
+    end
+    max_fish
 end
